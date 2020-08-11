53d6323aa75e392b0d3d435fbd883ae2f73e1515
Andrey <iandrewka@gmail.com>
2020-08-11 23:19:54 +0300

    refactor класса Student.kt

--- src/main/kotlin/com/homework/springdemo/mvc/controller/domain/Student.kt
+++ src/main/kotlin/com/homework/springdemo/mvc/controller/domain/Student.kt
@@ -1,17 +1,10 @@
 package com.homework.springdemo.mvc.controller.domain
 
-import org.springframework.beans.factory.annotation.Value
-import java.util.*
-
-
-@Value("#{countryOptions}")
-private val countryOptions: Map<String, String>? = null
 
 class Student {
     var firstName: String? = ""
     var lastName: String? = ""
     var country: String = ""
-    var countryOptions: LinkedHashMap<String, String> = linkedMapOf()
     var favoriteLanguage: String? = ""
     var operatingSystems = arrayListOf<String>()
 
@@ -21,8 +14,4 @@ class Student {
             "PHP" to "PHP",
             "Ruby" to "Ruby"
     )
-
-
-    constructor()
-
 }
